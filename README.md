1bc7ed48b2d875143f555bb5b8bae23bf185c9c9
Alexander Reinecke <a.reinecke@tu-bs.de>
2023-01-19 16:05:10 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# subdirterm
+
+## What is this?
+
+subdirterm is a script allowing you to quickly execute terminal commands 
+in subdirectories from a starting directory
+
+## Usage
+```
+python3 subdirterm <command>
+```
+
+## Options
+### Absoulute path
+If the option `-abs` is used, the next expression must be an absolute path to the starting directory.
+If this option is not present, the starting directory is the current working directory.
+
+## Prerequisites
+
+- Python 3
---
+++ subdirterm.py
@@ -0,0 +1,35 @@
+import os
+import sys
+
+
+# Get args
+args = sys.argv
+
+# Options
+optAbs = args[1] == "-abs"
+## Absolute path
+if optAbs:
+    os.chdir(args[2])
+    optAbs = True
+
+# Get command
+command = " ".join(args[3 if optAbs else 1:])
+
+# Get working directory
+cwd = os.getcwd()
+
+# Get list of subdirectories
+subdirectories = [d for d in os.listdir() if 
+    os.path.isdir(os.path.join(cwd, d))]
+
+print("Running " + command + " from following directories:")
+print(subdirectories)
+
+for subdir in subdirectories:
+    print("\n\n")
+    print(subdir + ":\n")
+
+    os.chdir(os.path.join(cwd, subdir))
+    os.system(command)
+
+print("\n\nDone")
