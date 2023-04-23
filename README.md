8789d0aebd8320169f2d78180bac5adebd40fb8f
Alexander Reinecke <a.reinecke@tu-bs.de>
2023-04-23 14:00:31 +0200

    File iteration update

--- README.md
+++ README.md
@@ -7,14 +7,16 @@ in subdirectories from a starting directory
 
 ## Usage
 ```
-python3 subdirterm <command>
+python3 subdirterm [-options] <command>
 ```
 
 ## Options
 ### Absoulute path
-If the option `-abs` is used, the next expression must be an absolute path to the starting directory.
+If the option `-a` is used, there must be an argument starting with `path=` giving an absolute path to the starting directory.
 If this option is not present, the starting directory is the current working directory.
 
+### File iteration
+`-f` Iterates through the file given by the argument starting with `file=` and replaces `§s` in the command with a line in the file
 ## Prerequisites
 
 - Python 3
--- subdirterm.py
+++ subdirterm.py
@@ -2,34 +2,77 @@ import os
 import sys
 
 
-# Get args
-args = sys.argv
+# Functions
+def run_in_subdirectories():
+    # Get working directory
+    cwd = os.getcwd()
 
-# Options
-optAbs = args[1] == "-abs"
-## Absolute path
-if optAbs:
-    os.chdir(args[2])
+    # Get list of subdirectories
+    subdirectories = [d for d in os.listdir() if
+                      os.path.isdir(os.path.join(cwd, d))]
+
+    print("Running " + command_from_args + " from following directories:")
+    print(subdirectories)
+    # Loop
+    for subdir in subdirectories:
+        print("\n\n")
+        print(subdir + ":\n")
+
+        os.chdir(os.path.join(cwd, subdir))
+        os.system(command_from_args)
+
+
+def run_file_iter():
+    print("Running iterations from file\n")
+    print(file_lines)
+    for line in file_lines:
+        print("\n")
+        run_cmd = command_from_args.replace("§s", line)
+        print("Running: ", run_cmd)
+        os.system(run_cmd)
+
+
+# Get args ------------------------------------------------
+
+args: list[str] = sys.argv
+cmd_start_index: int = 1
+
+# Options ---------------------------------------------
+
+# Absolute path
+opt_abs = args[1].__contains__("a")
+if opt_abs:
+    abs_path: str = ""
+    for word in args[2:]:
+        if word.startswith("path="):
+            abs_path = word.partition("=")[2]
+            break
+    os.chdir(abs_path)
+
+# File iteration
+opt_file_iter = args[1].__contains__("f")
+if opt_file_iter:
+    path_file: str = ""
+    for word in args[2:]:
+        if word.startswith("file="):
+            path_file = word.partition("=")[2]
+            break
+    file = open(path_file)
+    file_lines = file.read().split("\n")
+
+if opt_abs or opt_file_iter:
+    cmd_start_index += 2
+    if opt_abs and opt_file_iter:
+        cmd_start_index += 1
+
+# Execution
 
 # Get command
-command = " ".join(args[3 if optAbs else 1:])
+command_from_args = " ".join(args[cmd_start_index:])
 
-# Get working directory
-cwd = os.getcwd()
-
-# Get list of subdirectories
-subdirectories = [d for d in os.listdir() if 
-    os.path.isdir(os.path.join(cwd, d))]
-
-print("Running " + command + " from following directories:")
-print(subdirectories)
-
-# Loop
-for subdir in subdirectories:
-    print("\n\n")
-    print(subdir + ":\n")
-
-    os.chdir(os.path.join(cwd, subdir))
-    os.system(command)
+if opt_file_iter:
+    run_file_iter()
+else:
+    run_in_subdirectories()
 
 print("\n\nDone")
